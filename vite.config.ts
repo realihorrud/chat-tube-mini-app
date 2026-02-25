fe9478ae2b224bc24ee55afa09c1f8a1c723c988
Ihor <Igorryd7@gmail.com>
2026-02-25 11:04:46 +0200

    fix: remove server section

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import tseslint from "typescript-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -19,17 +18,4 @@ export default defineConfig({
     minify: "terser",
   },
   publicDir: "./public",
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost",
-        changeOrigin: true,
-      },
-    },
-    // Exposes your dev server and makes it accessible for the devices in the same network.
-    // host: true,
-    allowedHosts: [
-      "8cce-2a02-2378-10af-b63c-30c6-fb99-b200-38d1.ngrok-free.app",
-    ],
-  },
 });
